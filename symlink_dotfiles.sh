bbde8a78f9fcb96eddce98c8d02143d89f521d4b
Tristen Grant <hello@tristengrant.com>
2025-03-03 18:25:39 -0600

    updated symlink script to include xprofile file.

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -3,11 +3,12 @@
 # In the home directory
 ln -sf ~/Github/dotfiles/bashrc ~/.bashrc
 ln -sf ~/Github/dotfiles/bash_profile ~/.bash_profile
-ln -sf ~/Github/dotfiles/xinitrc ~/.xinitrc
+#ln -sf ~/Github/dotfiles/xinitrc ~/.xinitrc
 ln -sf ~/Github/dotfiles/Xresources ~/.Xresources
 ln -sf ~/Github/dotfiles/gtkrc-2.0 ~/.gtkrc-2.0
 ln -sf ~/Github/dotfiles/profile ~/.profile
 ln -sf ~/Github/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/Github/dotfiles/xprofile ~/.xprofile
 ln -sf ~/Github/dotfiles/xsession ~/.xsession
 
 # In the home/.config
