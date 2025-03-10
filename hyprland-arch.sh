95e4b413576656bfe044242d4290baec561cb34f
Tristen Grant <hello@tristengrant.com>
2025-03-09 22:13:35 -0500

    Update hyprland-arch.sh

--- hyprland-arch.sh
+++ hyprland-arch.sh
@@ -1,142 +1,4 @@
 #!/bin/bash
-set -e # Exit immediately if a command fails
-
-# Ensure script is run as root
-if [[ $EUID -ne 0 ]]; then
-  echo "This script must be run as root."
-  exit 1
-fi
-
-# Update the system clock
-echo "Setting system clock..."
-timedatectl set-ntp true
-
-# Set up pacman mirror list with reflector
-echo "Setting up the fastest mirrors..."
-
-# Use reflector to get the fastest mirrors for Canada (or your preferred country)
-reflector --country "Canada" --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
-
-# Update the system
-pacman -Syu --noconfirm
-
-# Partition the disks (assuming your system uses nvme0n1 for the main drive and sda for the extra SSD)
-echo "Partitioning the disks..."
-
-# Create partitions using sfdisk or any other partitioning tool (adjust according to your setup)
-# This is a rough template, adjust according to your actual setup
-# This will create the partition scheme on your nvme and sda
-(
-echo label gpt
-echo,1G,EFI
-echo,,linux
-) | sfdisk /dev/nvme0n1
-
-# Swap setup (use zram)
-SWAP_PARTITION="/dev/zram0"
-mkswap "$SWAP_PARTITION"
-swapon "$SWAP_PARTITION"
-
-# Create partition for /ssd
-echo "Creating partition for /ssd..."
-(
-echo label gpt
-echo,,linux
-) | sfdisk /dev/sda
-
-# Format the partitions (adjust fs type as needed)
-echo "Formatting the partitions..."
-mkfs.fat -F32 /dev/nvme0n1p1  # EFI partition
-mkfs.btrfs /dev/nvme0n1p2      # Main Btrfs partition
-mkfs.btrfs /dev/sda1            # /ssd Btrfs partition
-
-# Mount the root filesystem
-echo "Mounting the root filesystem..."
-mount /dev/nvme0n1p2 /mnt
-
-# Create subvolumes for Btrfs
-echo "Creating Btrfs subvolumes..."
-btrfs subvolume create /mnt/@
-btrfs subvolume create /mnt/@home
-btrfs subvolume create /mnt/@var
-btrfs subvolume create /mnt/@tmp
-btrfs subvolume create /mnt/@log
-btrfs subvolume create /mnt/@snapshots
-
-# Mount the subvolumes
-umount /mnt
-mount -o noatime,compress=lzo,subvol=@ /dev/nvme0n1p2 /mnt
-mkdir /mnt/{boot,home,var,tmp,log,.snapshots}
-mount -o noatime,compress=lzo,subvol=@home /dev/nvme0n1p2 /mnt/home
-mount -o noatime,compress=lzo,subvol=@var /dev/nvme0n1p2 /mnt/var
-mount -o noatime,compress=lzo,subvol=@tmp /dev/nvme0n1p2 /mnt/tmp
-mount -o noatime,compress=lzo,subvol=@log /dev/nvme0n1p2 /mnt/log
-mount -o noatime,compress=lzo,subvol=@snapshots /dev/nvme0n1p2 /mnt/.snapshots
-
-# Mount the /ssd drive to /ssd
-echo "Mounting /dev/sda1 to /ssd..."
-mkdir /mnt/ssd
-mount /dev/sda1 /mnt/ssd
-
-# Set up the EFI partition
-mount /dev/nvme0n1p1 /mnt/boot
-
-# Install base system
-echo "Installing base system..."
-pacstrap /mnt base linux linux-firmware btrfs-progs vim networkmanager reflector git sudo base-devel
-
-# Generate fstab
-echo "Generating fstab..."
-genfstab -U /mnt >> /mnt/etc/fstab
-
-# Chroot into the new system
-echo "Chrooting into the new system..."
-arch-chroot /mnt
-
-# Set time zone
-echo "Setting time zone..."
-ln -sf /usr/share/zoneinfo/Etc/UTC /etc/localtime
-hwclock --systohc
-
-# Localization
-echo "Setting up localization..."
-echo "en_CA.UTF-8 UTF-8" > /etc/locale.gen
-locale-gen
-echo "LANG=en_CA.UTF-8" > /etc/locale.conf
-echo "KEYMAP=us" > /etc/vconsole.conf
-
-# Set hostname
-echo "Setting hostname..."
-echo "arch" > /etc/hostname
-
-# Set up root password
-echo "Setting root password..."
-passwd
-
-# Add user "tristen"
-echo "Creating user 'tristen'..."
-useradd -m -G wheel -s /bin/bash tristen
-echo "Set password for user 'tristen'..."
-passwd tristen
-
-# Grant sudo privileges
-echo "Setting up sudo privileges..."
-echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
-
-# Install and configure bootloader (GRUB for UEFI)
-echo "Installing GRUB bootloader..."
-pacman -S --noconfirm grub efibootmgr
-
-# Install GRUB to the EFI partition
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-
-# Create the GRUB configuration
-echo "Generating GRUB config..."
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# Enable necessary services
-echo "Enabling services..."
-systemctl enable NetworkManager
 
 # Install paru if not installed
 if ! command -v paru &>/dev/null; then
@@ -346,19 +208,7 @@ cd /home/tristen/Pictures
 git clone https://github.com/tristengrant/wallpapers.git
 cd ~
 
-# Clone your website repository
-echo "Cloning your website repository..."
-cd /home/tristen/Github
-git clone https://github.com/tristengrant/tristengrant.com.git tristengrant
-cd ~
-
-# Clone your comic site repository
-echo "Cloning your comic website repository..."
-cd /home/tristen/Github
-git clone https://github.com/tristengrant/catandbotcomic.com.git catandbot
-cd ~
-
-# Clone your website repository
+# Clone your github introduction repository
 echo "Cloning your website repository..."
 cd /home/tristen/Github
 git clone https://github.com/tristengrant/tristengrant.git readme
@@ -398,12 +248,6 @@ for grp in tty realtime video audio input; do
   sudo usermod -aG "$grp" "$USER"
 done
 
-# Update Krita
-cd ~/Scripts && ./update_krita.sh
-
-# Enable services
-sudo systemctl enable --now cups cronie
-
 # Define directories
 APP_DIR="/home/tristen/Applications"
 DESKTOP_DIR="$HOME/.local/share/applications"
@@ -440,10 +284,6 @@ EOF
 
 chmod +x "$DESKTOP_ENTRY"
 
-# Force update the desktop menu to ensure Krita appears
-echo "Updating desktop menu..."
-xdg-desktop-menu forceupdate
-
 echo "Krita $LATEST_VERSION installed successfully!"
 echo "Run it with: $APPIMAGE_PATH or searching with your application menu."
 
@@ -451,12 +291,4 @@ echo "Run it with: $APPIMAGE_PATH or searching with your application menu."
 systemctl enable ly.service
 
 # Reboot
-echo "Installation completed. Rebooting..."
-reboot
-
-
-
-
-
-
-
+echo "Installation completed. Please reboot..."
