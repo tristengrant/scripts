47402d14b3cbf9aa95ceec0da27840fb76b5054a
Tristen Grant <hello@tristengrant.com>
2025-03-11 01:53:50 -0500

    Updated.

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -2,19 +2,14 @@
 
 # In the home directory
 ln -sf ~/Github/dotfiles/bashrc ~/.bashrc
-ln -sf ~/Github/dotfiles/bash_profile ~/.bash_profile
-#ln -sf ~/Github/dotfiles/xinitrc ~/.xinitrc
-ln -sf ~/Github/dotfiles/Xresources ~/.Xresources
 ln -sf ~/Github/dotfiles/gtkrc-2.0 ~/.gtkrc-2.0
-ln -sf ~/Github/dotfiles/profile ~/.profile
-ln -sf ~/Github/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/Github/dotfiles/xprofile ~/.xprofile
 ln -sf ~/Github/dotfiles/xprofile ~/.xprofile
 ln -sf ~/Github/dotfiles/xsession ~/.xsession
 
 # In the home/.config
 ln -sf ~/Github/dotfiles/fontconfig ~/.config
 ln -sf ~/Github/dotfiles/gtk-3.0 ~/.config
-ln -sf ~/Github/dotfiles/kitty ~/.config
 ln -sf ~/Github/dotfiles/QtProject.conf ~/.config
 ln -sf ~/Github/dotfiles/qt5ct ~/.config
 ln -sf ~/Github/dotfiles/Kvantum ~/.config
