c45e30a80f3302ad4ecefaa496a5d1ce869ccc54
Tristen Grant <tgrant@fastmail.ca>
2025-11-15 02:10:57 -0600

    Cleanup

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # In the home directory
-ln -sf ~/Github/dotfiles/dot_bashrc ~/.bashrc
-ln -sf ~/Github/dotfiles/dot_bash_aliases ~/.bash_aliases
-ln -sf ~/Github/dotfiles/dot_nanorc ~/.nanorc
-ln -sf ~/Github/dotfiles/dot_xinitrc ~/.xinitrc
+ln -sf ~/Projects/dotfiles/dot_bashrc ~/.bashrc
+ln -sf ~/Projects/dotfiles/dot_bash_aliases ~/.bash_aliases
+ln -sf ~/Projects/dotfiles/dot_nanorc ~/.nanorc
+ln -sf ~/Projects/dotfiles/dot_xinitrc ~/.xinitrc
 
 # In the home/.config
-ln -sf ~/Github/dotfiles/nvim ~/.config
+ln -sf ~/Projects/dotfiles/mpv ~/.config/mpv
